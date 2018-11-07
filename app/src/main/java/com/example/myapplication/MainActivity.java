86100d289828a8d27f9f9be3401cc7ec471daa9a
Zydrune <zydrunemac@gmail.com>
2018-11-07 09:18:06 +0200

    ArrayList metodas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,16 +6,23 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText name_txt, lname_txt, email_txt, tel_txt,birthday_txt;
-    Persona Master;
+    ArrayList<Persona>sarasas = new ArrayList<Persona>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Master = new Persona();
+        name_txt = findViewById(R.id.name_txt);
+        lname_txt = findViewById(R.id.lname_txt);
+        email_txt = findViewById(R.id.email_txt);
+        tel_txt = findViewById(R.id.tel_txt);
+        birthday_txt = findViewById(R.id.birthday_txt);
+
     }
 }
